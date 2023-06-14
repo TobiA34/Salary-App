d80f949950effb9632b5ee32bbea1f794d4da0e3
“TobiA34” <tobiadegoroye49@gmail.com>
2023-06-14 18:55:46 +0100

    Change from storyboard to be programmatic Added code to format the currency Added navigation title Added new computed variables

--- Salary App/ViewController.swift
+++ Salary App/ViewController.swift
@@ -9,32 +9,89 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var salaryTextField: UITextField!
-    
-    @IBOutlet weak var salarySC: UISegmentedControl!
-    
-    @IBOutlet weak var monthlySalaryLbl: UILabel!
-    @IBOutlet weak var weeklySalaryLbl: UILabel!
-    // create variable monthly, days, weekly
-    
-    @IBOutlet weak var dailySalaryLbl: UILabel!
-    
     //Annual salary ÷ 52 = weekly rate
     // (Annual salary ÷ 52)÷ 7.5 = Hourly rate
     //Annual salary ÷ 12 = Monthly rate
     
-    var monthlySalary: Int?
-    var weeklySalary: Int?
-    var dailySalary: Int?
+    var monthlySalary: Decimal?
+    var weeklySalary: Decimal?
+    var dailySalary: Decimal?
     
-    let defaults = UserDefaults.standard
+    lazy var salaryTextField: UITextField = {
+        let  salaryTextField = UITextField()
+        salaryTextField.placeholder = "Please enter salary"
+        salaryTextField.layer.borderWidth = 1
+        salaryTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        salaryTextField.layer.borderColor = UIColor.black.cgColor
+        salaryTextField.textColor = .black
+        salaryTextField.layer.cornerRadius = 16
+        salaryTextField.keyboardType = .asciiCapableNumberPad
+        salaryTextField.translatesAutoresizingMaskIntoConstraints = false
+        salaryTextField.delegate = self
+        return salaryTextField
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    lazy var monthlySalaryLbl: UILabel = {
+        let  monthlySalaryLbl = UILabel()
+        monthlySalaryLbl.textColor = .black
+        monthlySalaryLbl.text = "Monthly Total: £0 "
+        monthlySalaryLbl.translatesAutoresizingMaskIntoConstraints = false
+        return monthlySalaryLbl
+    }()
+    
+    lazy var weeklySalaryLbl: UILabel = {
+        let  weeklySalaryLbl = UILabel()
+        weeklySalaryLbl.textColor = .black
+        weeklySalaryLbl.text = "Weekly Total: £0"
+        weeklySalaryLbl.translatesAutoresizingMaskIntoConstraints = false
+        return weeklySalaryLbl
+    }()
+    
+    
+    lazy var dailySalaryLbl: UILabel = {
+        let  dailySalaryLbl = UILabel()
+        dailySalaryLbl.textColor = .black
+        dailySalaryLbl.text = "Daily Total: £0"
+        dailySalaryLbl.translatesAutoresizingMaskIntoConstraints = false
+        return dailySalaryLbl
+    }()
+    
+    lazy var submitBtn: UIButton = {
+        var button = UIButton(type: .system)
+        var config = UIButton.Configuration.filled()
+        button.configuration = .filled()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        config.cornerStyle = .capsule
+        button.configuration = config
+        return button
+    }()
+    
+    
+    private func addPadding() {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 50))
+        salaryTextField.leftView = paddingView
+        salaryTextField.leftViewMode = .always
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        salaryTextField.keyboardType = .asciiCapableNumberPad
-        salaryTextField.delegate = self
+        view.backgroundColor = .white
+        self.navigationItem.title = "My Salary Calculator"
+        navigationController?.navigationBar.prefersLargeTitles = true
         setDoneOnKeyboard()
+        addPadding()
+        setScreenLayout()
     }
     
     func setDoneOnKeyboard() {
@@ -45,63 +102,83 @@ class ViewController: UIViewController, UITextFieldDelegate {
         keyboardToolbar.items = [flexBarButton, doneBarButton]
         self.salaryTextField.inputAccessoryView = keyboardToolbar
     }
-
+    
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
     
+    func setScreenLayout() {
+        
+        submitBtn.setTitle("Calculate Salary", for: .normal)
+        submitBtn.addTarget(self, action: #selector(submitBtnPressed), for: .touchUpInside)
+        
+        view.addSubview(salaryTextField)
+        view.addSubview(submitBtn)
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(monthlySalaryLbl)
+        stackView.addArrangedSubview(weeklySalaryLbl)
+        stackView.addArrangedSubview(dailySalaryLbl)
+        
+        
+        NSLayoutConstraint.activate([
+            salaryTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 20),
+            salaryTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 16),
+            salaryTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -16),
+            salaryTextField.heightAnchor.constraint(equalToConstant: 50),
+            salaryTextField.widthAnchor.constraint(equalToConstant: 100),
+            
+            stackView.topAnchor.constraint(equalTo: salaryTextField.safeAreaLayoutGuide.topAnchor,constant: 84),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 48),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -48),
+            stackView.heightAnchor.constraint(equalToConstant: 200),
+            
+            
+            submitBtn.topAnchor.constraint(equalTo: stackView.safeAreaLayoutGuide.topAnchor,constant: 220),
+            submitBtn.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 48),
+            submitBtn.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -48),
+            submitBtn.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
     func presentError() {
         
-        if salaryTextField.text == "" {
-            let alert = UIAlertController(title: "Salary", message: "You need to enter yearly", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "dismiss", style: .default))
-            self.present(alert, animated: true, completion: nil)
-        }
+        let alert = UIAlertController(title: "Salary", message: "You need to enter yearly", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "dismiss", style: .default))
+        self.present(alert, animated: true, completion: nil)
     }
-
     
     
-    @IBAction func submitBtnPressed(_ sender: Any) {
+    
+    @objc func submitBtnPressed() {
         
-        presentError()
+        let salary = Decimal(string:self.salaryTextField.text!) ?? 0.0
         
-        switch salarySC.selectedSegmentIndex {
-        case 0:
-            let salary = Int(self.salaryTextField.text!) ?? 0
-            monthlySalary =  salary / 12
-            
-            let formattedMonthlySalary = " £\(String(monthlySalary ?? 0))"
-            
-            if salary != 0 {
-                monthlySalaryLbl.text = "This is your monthly salary:\(formattedMonthlySalary)"
-            }
-            break
-            
-        case 1:
-            let salary = Int(self.salaryTextField.text!) ?? 0
-            dailySalary =  (salary / 52) / Int(7.5)
-            let formattedHourlySalary = " £\(String(dailySalary ?? 0))"
-            
-            if salary != 0 {
-                dailySalaryLbl.text = "This is your daily salary:\(formattedHourlySalary)"
-            }
-            break
-            
-        case 2:
-            let salary = Int(self.salaryTextField.text!) ?? 0
-            weeklySalary =  (salary / 52)
-            defaults.set(weeklySalary, forKey: "weekly")
-            
-            let formattedWeeklySalary = " £\(String(weeklySalary ?? 0))"
-            if salary != 0 {
-                weeklySalaryLbl.text = "This is your weekly salary:\(formattedWeeklySalary)"
-            }
-
-            break
-            
-        default:
-            break
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 0
+        formatter.currencyCode = "GBP"
+        formatter.numberStyle = .currency
+        
+        if salaryTextField.text == "" {
+            presentError()
         }
+        
+        if salary != 0 {
+            
+            monthlySalary =  salary / 12.0
+            dailySalary =  (salary / 52.0) / 7.5
+            weeklySalary =  (salary / 52.0)
+            
+            let formattedMonthlySalary =  formatter.string(for: monthlySalary) ?? "?"
+            let formattedWeeklySalary =  formatter.string(for: weeklySalary) ?? "?"
+            let formattedDailySalary =  formatter.string(for: dailySalary) ?? "?"
+            
+            monthlySalaryLbl.text = "Monthly Total: \(formattedMonthlySalary)"
+            weeklySalaryLbl.text = "Weekly Total: \(formattedWeeklySalary)"
+            dailySalaryLbl.text = "Daily Total: \(formattedDailySalary)"
+            
+        }
+        
     }
     
 }
